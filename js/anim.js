a62152785e4af90b3b2ee5590c553a71e4cc1410
Laide Lawal <104867707+LaideLawal@users.noreply.github.com>
2022-10-05 22:48:54 +0100

    Add files via upload

---
+++ js/anim.js
@@ -0,0 +1,18 @@
+
+let start = Date.now();
+
+let timer = setInterval(() => {
+
+    let timePassed= Date.now() - start;
+
+    if(timePassed >= 2000){
+        clearInterval(timer);
+        return;
+    }
+    draw(timePassed);
+    
+}, 20);
+
+function draw(timePassed){
+    train.style.left = timePassed /5 + "px";
+}
---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+const cursor = document.querySelector(".cursor");
+
+var timeout;
+
+//follow cursor on mousemove
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+
+  //cursor effects when mouse stopped
+  function mouseStopped() {
+    cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+});
+
+//cursor effects when mouseout
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
+
+
+
+
+document.addEventListener("wheel", (e) =>{
+
+var id = null;
+
+  var elem = document.querySelector("central-img");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.width = pos + 'px'; 
+      elem.style.height = pos + 'px'; 
+    }
+  }
+});
+
+
